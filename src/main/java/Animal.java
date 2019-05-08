a01afd5684197c63611dcbe8c523cf6d6ffa62e6
Jake Torgerson <44759116+wackyvert@users.noreply.github.com>
2019-05-08 11:31:27 -0500

    First Commit

---
+++ src/main/java/Animal.java
@@ -0,0 +1,13 @@
+public class Animal {
+    public void walk (double distanceInMeters){
+        System.out.println("I just walked "+distanceInMeters+" meters.");
+    }
+    public void eat (String food){
+        System.out.println("I just ate "+".");
+    }
+    public void sleep(int time){
+        System.out.println("I am going to sleep, goodnight.");
+        sleep(2000);
+        System.out.println("Good morning!");
+    }
+}
---
+++ src/main/java/Bird.java
@@ -0,0 +1,5 @@
+public class Bird extends Animal {
+    public void fly(double distanceInMeters){
+        System.out.println("I just flew "+distanceInMeters+" meters.");
+    }
+}
